61fca8c67cadf32f21f36ac2d19f557c23741347
saroj8455 <saroj.padhan11@gmail.com>
2018-08-12 23:59:59 +0530

    Add some new line

--- variable.php
+++ variable.php
@@ -38,6 +38,47 @@ $x=200;
 $y=300;
 print $x;
 
+//Print Statements /Output Statements
+
+$a=150;
+$b=75;
+$name="scoty";
+echo "<br><br>";
+//print() and echo() method
+print $a."<br>"; //output 150
+print print $a."<br>"; //outpu 1150
+                    //print method will always return 'true' after print output so that output will be  1150
+
+echo $a."<br>"; // output 150
+
+$var_one=(print_r($b));
+echo $var_one;
+echo "<br><br>";
+echo var_dump($b); //output int(75) //to know the type of variable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
